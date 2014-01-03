c38ba54f7de9761b728a0036e2dc45a3c5519718
yimin <yimin@yimin-PC>
2014-01-02 18:34:21 -0800

    added more data operation methods

--- src/main/java/com/bedrosians/bedlogic/dao/GenericDao.java
+++ src/main/java/com/bedrosians/bedlogic/dao/GenericDao.java
@@ -3,10 +3,18 @@ package com.bedrosians.bedlogic.dao;
 import java.io.Serializable;
 import java.util.List;
 
+import com.bedrosians.bedlogic.util.PatternMatchMode;
+import com.bedrosians.bedlogic.util.RestrictionOperation;
+
 public interface GenericDao <T, PK extends Serializable>{
-	PK create(T newInstance);
+	PK save(T newInstance);
 	T read(PK id);
 	List<T> readMultipleRecords(PK id);
 	void update(T transientObject);
 	void delete(T persistentObject);
+	List<T> readByParameter(String parameterName, String value);
+	List<T> readByParameter(String parameterName, Long value);
+	List<T> readByParameter(String parameterName, String value, RestrictionOperation op);
+	List<T> readByParameterPatternMatch(String parameterName, String value, PatternMatchMode matchMode);
+
 }
--- src/main/java/com/bedrosians/bedlogic/dao/GenericDaoImpl.java
+++ src/main/java/com/bedrosians/bedlogic/dao/GenericDaoImpl.java
@@ -4,10 +4,17 @@ import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.exception.DataException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.bedrosians.bedlogic.util.PatternMatchMode;
+import com.bedrosians.bedlogic.util.RestrictionOperation;
 
 
 
@@ -20,7 +27,6 @@ public class GenericDaoImpl<T, PK extends Serializable> implements GenericDao<T,
 	
 	protected Session currentSession() {
 	    return sessionFactory.getCurrentSession();
-	//	return sessionFactory.openSession();
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -28,11 +34,11 @@ public class GenericDaoImpl<T, PK extends Serializable> implements GenericDao<T,
 		this.type = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
 	}
 	
-	public PK create(T newInstance) {
-		return null;
+	public PK save(T newInstance) {
+		return (PK)currentSession().save(newInstance);
 	}
 	@SuppressWarnings("unchecked")
-	public T read(PK id) {
+	public T read(final PK id) {
 		return (T)currentSession().get(type, id);
 	}
 	
@@ -40,10 +46,87 @@ public class GenericDaoImpl<T, PK extends Serializable> implements GenericDao<T,
     public List<T> readMultipleRecords(PK id) {
 		return (List<T>)currentSession().get(type, id);
 	}
-	public void update(T transientObject) {
-		
-	}
+	
+	@Transactional
+	public synchronized void update(final T transientObject) {
+		try{
+           Session session = currentSession();
+           if(session.contains(transientObject))
+        	  session.saveOrUpdate(transientObject);
+           else
+        	  session.merge(transientObject);
+		}
+		catch(DataException e){
+			throw e;
+		}
+		   
+   	}
+	
 	public void delete(T persistentObject) {
 		
 	}
+	
+	@Override
+    public List<T> readByParameter(final String parameterName, String value){
+		if(value != null && value.length() > 0)
+		   value = value.toUpperCase();
+	  	Criteria criteria = currentSession().createCriteria(type);
+	  	criteria.setReadOnly(true);
+	  	criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+		criteria.add(Restrictions.eq(parameterName, value));
+		return (List<T>)criteria.list();			
+	}
+	
+	@Override
+    public List<T> readByParameter(final String parameterName, String value, final RestrictionOperation op){
+		if(value != null && value.length() > 0)
+		   value = value.toUpperCase();
+	  	Criteria criteria = currentSession().createCriteria(type);
+	  	criteria.setReadOnly(true);
+	  	criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+	  	switch(op) {
+	  	  case EQ:
+	  		   criteria.add(Restrictions.eq(parameterName, value));  	
+	  	  case NE:
+	  		   criteria.add(Restrictions.ne(parameterName, value));
+	  	  case IN:
+	  		   criteria.add(Restrictions.in(parameterName, value.split(",")));	   
+	  		   break;
+	  	case NONE:
+	  		   break;	   
+	  	  default:
+	  		   break;
+	  	}		   
+		return (List<T>)criteria.list();			
+	}
+	
+	@Override
+    public List<T> readByParameter(final String parameterName, Long value){
+	  	Criteria criteria = currentSession().createCriteria(type);
+	  	criteria.setReadOnly(true);
+	  	criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+		criteria.add(Restrictions.eq(parameterName, value));
+		return (List<T>)criteria.list();			
+	}
+	
+	@Override
+    public List<T> readByParameterPatternMatch(final String parameterName, String value, final PatternMatchMode matchMode){
+	  	Criteria criteria = currentSession().createCriteria(type);
+	  	switch(matchMode){
+	  		case START:
+	  			criteria.add(Restrictions.like(parameterName, value.toUpperCase(), MatchMode.START));
+		        break;
+	  		case END:
+	  			criteria.add(Restrictions.like(parameterName, value.toUpperCase(), MatchMode.END));
+			    break;
+	  		case ANYWHERE:
+	  			criteria.add(Restrictions.like(parameterName, value.toUpperCase(), MatchMode.ANYWHERE));
+			    break;
+			default:
+				criteria.add(Restrictions.like(parameterName, value.toUpperCase(), MatchMode.EXACT));
+	  	} 				
+	  	criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+	  	return (List<T>)criteria.list();
+			
+	 }
 }
