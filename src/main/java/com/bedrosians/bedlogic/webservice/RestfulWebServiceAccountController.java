f3fa73fb8e855988095f67e4069882921c46419e
yimin <yimin@yimin-PC>
2014-01-06 16:04:24 -0800

    modified some methods with account lookup

--- src/main/java/com/bedrosians/bedlogic/webservice/RestfulWebServiceAccountController.java
+++ src/main/java/com/bedrosians/bedlogic/webservice/RestfulWebServiceAccountController.java
@@ -3,10 +3,6 @@ package com.bedrosians.bedlogic.webservice;
 import java.util.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
