63034ebcf0f6d9800543476fa4421b94996ad189
yimin <yimin@yimin-PC>
2014-01-15 17:43:22 -0800

    new class

---
+++ src/main/java/com/bedrosians/bedlogic/domain/account/AccountBranchId.java
@@ -0,0 +1,70 @@
+package com.bedrosians.bedlogic.domain.account;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.bedrosians.bedlogic.util.FormatUtil;
+
+@XmlRootElement(name="accountBrancheIds")
+@Entity
+@Table(name="armbr")
+public class AccountBranchId implements Serializable {
+	
+	private static final long serialVersionUID = 5137707010951170389L;
+
+	@EmbeddedId
+	private BranchPK branchPK;
+				
+	/*@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "custcd", nullable = false)
+	public String getAccountId() {
+		return FormatUtil.trimAndReplicate(branchPK.getAccountId());
+	}
+    */
+	
+	public String getBranchId() {
+		return FormatUtil.trimAndReplicate(branchPK.getBranchId());
+	}
+	
+		
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((branchPK == null) ? 0 : branchPK.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AccountBranchId other = (AccountBranchId) obj;
+		if (branchPK == null) {
+			if (other.branchPK != null)
+				return false;
+		} else if (!branchPK.equals(other.branchPK))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "branchId=" + getBranchId();
+	}
+	
+	
+}
