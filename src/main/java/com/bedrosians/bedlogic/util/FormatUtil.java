f968b8cbaa68ebdb0f3c1340ecd60298d4f11b79
yimin <yimin@yimin-PC>
2014-01-15 17:46:06 -0800

    modifed to handle null value

--- src/main/java/com/bedrosians/bedlogic/util/FormatUtil.java
+++ src/main/java/com/bedrosians/bedlogic/util/FormatUtil.java
@@ -3,7 +3,11 @@ package com.bedrosians.bedlogic.util;
 public class FormatUtil {
 
 	public static String trimAndReplicate(String oriString){
-		if (oriString != null) oriString = oriString.trim(); 
-		return new String(oriString);
+		if (oriString == null)
+			return "";
+		else {
+			oriString = oriString.trim(); 
+		    return new String(oriString);
+		}    
 	}
 }
