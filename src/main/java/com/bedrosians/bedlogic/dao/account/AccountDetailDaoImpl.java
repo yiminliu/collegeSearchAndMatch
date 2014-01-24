e37c27a06a0fe9116e9b0976a953b8dad5921916
yimin <yimin@yimin-PC>
2014-01-23 16:28:52 -0800

    Creaetd new dao classes to handle db operations for simplified and detailed account and branche classes

---
+++ src/main/java/com/bedrosians/bedlogic/dao/account/AccountDetailDaoImpl.java
@@ -0,0 +1,28 @@
+package com.bedrosians.bedlogic.dao.account;
+
+
+import org.springframework.stereotype.Repository;
+
+import com.bedrosians.bedlogic.dao.GenericDaoImpl;
+import com.bedrosians.bedlogic.domain.account.AccountDetail;
+
+
+@Repository("accountDetailDao")
+public class AccountDetailDaoImpl extends GenericDaoImpl<AccountDetail, String> implements AccountDetailDao {
+						
+	@Override
+	public AccountDetail getAccountById(String accountId) {
+		//return findById(accountId);//branch info cannot be linked to the account.
+		return findByParameter("accountId", accountId).get(0);
+	}
+	
+	@Override
+	public void updateAccount(AccountDetail account){
+		update(account);
+	}
+	  
+	@Override
+	public String createAccount(AccountDetail account){
+		return save(account);  
+	}
+}
