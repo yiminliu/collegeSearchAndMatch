476f377acec1d2411dfb7d517dd902e6007de271
yimin <yimin@yimin-PC>
2014-01-29 14:20:41 -0800

    Set the updated lastcustcd field to the obmh table

--- src/main/java/com/bedrosians/bedlogic/util/IdGenerator.java
+++ src/main/java/com/bedrosians/bedlogic/util/IdGenerator.java
@@ -1,8 +1,6 @@
 package com.bedrosians.bedlogic.util;
 
 import java.io.Serializable;
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.hibernate.Query;
@@ -26,47 +24,11 @@ public class IdGenerator implements IdentifierGenerator{
   public Serializable generate(SessionImplementor session,  Object object) throws DataAccessException, UnsupportedOperationException {
 	 long newId = 0l;
 	 Query query = session.getNamedQuery("findLastCustCd");
-	 try{
-	    session.connection().setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
-	  
-	 Obmh record = (Obmh)query.uniqueResult();
-	 if (record != null)
-	    newId = new AtomicLong((Long)record.getLastCustId()).incrementAndGet(); 
-	 }catch(SQLException sqle){
-		 session.getTransactionCoordinator().getTransaction().rollback();
-	 }	
-	 finally{
-		 try{
-		    session.connection().setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-		 }catch(SQLException sqle){
-		 }   
-	 }
+	 Obmh obmhRecord = (Obmh)query.uniqueResult();
+	 if (obmhRecord != null)
+	    newId = new AtomicLong((Long)obmhRecord.getLastCustId()).incrementAndGet(); 
+	 obmhRecord.setLastCustId(newId);	 
 	 return String.valueOf(newId);
- }
+ }  
   
-  //@Transactional
- /* private long fetchLastUserId() throws DataAccessException, UnsupportedOperationException {
-    try {
-      //List<Integer> list = getHibernateTemplate().find("select max(user.userId) from User user");
-      	Query query = currentSession().createQuery("from Obmh");
-    	Obmh record = (Obmh)query.uniqueResult();
-    	if ((record == null)) {
-             return 0;
-        }
-        return (Long)record.getLastCustId();
-    }
-    catch (DataAccessException dae)
-    {
-      throw dae;
-    }
-    catch (ClassCastException cce)
-    {
-      throw new UnsupportedOperationException("Only numeric type user id filed is allowed!");
-    }
-    catch (Exception e)
-    {
-      throw new RuntimeException("Exception occured : " + e.getMessage() + ".  Unable to determin user id.");
-    }
-  }*/  
-  
- }
+}
