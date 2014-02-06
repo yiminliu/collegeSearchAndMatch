19edfcbde178d928fa07457351fcc1706070764d
yimin <yimin@yimin-PC>
2014-02-06 14:11:58 -0800

    Code cleaning up

--- src/main/java/com/bedrosians/bedlogic/dao/AccountBranchDao.java
+++
@@ -1,8 +0,0 @@
-package com.bedrosians.bedlogic.dao;
-
-import com.bedrosians.bedlogic.domain.AccountBranch;
-import com.bedrosians.bedlogic.domain.BranchPK;
-
-public interface AccountBranchDao extends GenericDao<AccountBranch, BranchPK> {
-
-}
--- src/main/java/com/bedrosians/bedlogic/dao/CheckPaymentDao.java
+++
@@ -1,9 +0,0 @@
-package com.bedrosians.bedlogic.dao;
-
-import java.util.List;
-
-import com.bedrosians.bedlogic.domain.CheckPayment;
-
-public interface CheckPaymentDao {
-	List<CheckPayment> getCheckPaymentsForAccount(String custcd);
-}
