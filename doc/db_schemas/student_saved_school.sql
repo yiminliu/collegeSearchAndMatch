4bd47f8903f1910b7e12734b6c5786be4557702a
yliu <yimin_l@hotmail.com>
2015-12-12 21:38:46 -0800

    more data

--- doc/db_schemas/student_saved_school.sql
+++ doc/db_schemas/student_saved_school.sql
@@ -4,9 +4,10 @@ CREATE TABLE student_saved_school
 (
   student_id INT NOT NULL,
   school_id INT NOT NULL,
+  saved_date DATE,
   PRIMARY KEY (student_id, school_id),
   FOREIGN KEY (student_id) REFERENCES student (student_id),
-  FOREIGN KEY (school_id) REFERENCES school (school_id)
+  FOREIGN KEY (school_id) REFERENCES school (id)
 );
 
 Commit;
